13756ad81a158ed70f411a359c437c4aefa7f609
aliksalot <alexkolev05@gmail.com>
2023-08-06 12:35:51 +0300

    Some changes to book page

--- src/controllers/serve_pages.js
+++ src/controllers/serve_pages.js
@@ -11,6 +11,11 @@ const serveHeader = (req, res) => {
     res.sendFile(path.join(__dirname, '../public/html/header.html'))
 }
 
+const serveBookTemp = (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/html/book.html'))
+}
+
+router.get('/book', serveBookTemp)
 router.get('/home', serveHome)
 router.get('/header', serveHeader)
 
